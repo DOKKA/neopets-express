52c0c6b7edc307af816a209b27610bf7b8e2ecc7
Kevin Grieger <scuffmark@gmail.com>
2018-06-22 01:19:01 -0500

    added redis

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,24 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 var Promise = require("bluebird");
 
+var redis = require('redis');
+var client = redis.createClient();
+
+function getAsync(key){
+    return new Promise((resolve, reject)=>{
+        client.get(key, (err, result)=>{
+            if(err){
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+}
+
 function transform(body) {
   return cheerio.load(body);
 }
-
-
 
 function getOptions(searchTerm){
   return {
@@ -21,31 +34,43 @@ function getOptions(searchTerm){
   }
 }
 
+function mapFn(searchTerm){
+    return getAsync(searchTerm).then((value)=>{
+        if(value != null){
+            return {
+                price: value,
+                item: searchTerm
+            };
+        } else {
+            return rp(getOptions(searchTerm)).then(function ($) {
+                var price = $('.price-history-link').text();
+                client.set(searchTerm, price, 'EX', 86400);
+                return {
+                    price: price,
+                    item: searchTerm
+                }
+            })
+            .catch(function (err) {
+                return {
+                    item: searchTerm
+                }
+            });
+        }
+    });
+    
+}
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/prices', function(req, res, next) {
-  //console.log(req.body)
-  Promise.map(req.body['items[]'], (searchTerm)=>{
-    return rp(getOptions(searchTerm))
-     .then(function ($) {
-         var price = $('.price-history-link').text();
-         return {
-             price: price,
-             item: searchTerm
-         }
-     })
-     .catch(function (err) {
-         return {
-             item: searchTerm
-         }
-     });
- 
- }, {concurrency: 3}).then((obj)=>{
+  Promise.map(req.body['items[]'], mapFn, {concurrency: 3}).then((obj)=>{
    res.json(obj)
- })
+ });
 });
 
 module.exports = router;
